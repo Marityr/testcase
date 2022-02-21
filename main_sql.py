1a01306697f61ddb9e7c823aa83d437c9f611ad7
Marityr <dmitriy.woronow@gmail.com>
2022-02-21 19:12:50 +0300

    Update main_sql.py

--- main_sql.py
+++ main_sql.py
@@ -16,9 +16,10 @@ try:
                                   database=os.getenv('NAME_DB'))
 
     cursor = connection.cursor()
-    create_table_query = '''create table images(id int, image bytea);'''
-    record_table_file = '''insert into images values (1, pg_read_binary_file('/path/to/image.jpg')::bytea);'''
-    # Выполнение команды: это создает новую таблицу
+    
+    create_table_query = '''create table files(id int, image bytea);'''
+    record_table_file = '''insert into files values (1, pg_read_binary_file('/path/to/image.jpg')::bytea);'''
+    
     cursor.execute(create_table_query)
     cursor.execute(record_table_file)
     connection.commit()
